f03b841eaf1185f07c1635cc60efeae7a466a789
Tristan Lee <tristanlee85@gmail.com>
2020-12-17 09:43:07 -0500

    Initial commit

---
+++ routes.js
@@ -0,0 +1,10 @@
+import { Router } from '@xdn/core/router';
+
+export default new Router().match('/', ({ serveStatic }) => {
+  serveStatic('./index.html');
+});
+
+// uncomment to get /__xdn__/cache-manifest.js to fail
+// .fallback(({ appShell }) => {
+//   appShell('./index.html');
+// });
