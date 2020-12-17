f9d7bfcf7f4381cea6c9be7c3b6e1b9b392e8e53
Tristan Lee <tristan.lee@moovweb.com>
2020-12-17 09:44:37 -0500

    test

--- routes.js
+++ routes.js
@@ -1,10 +1,11 @@
 import { Router } from '@xdn/core/router';
 
-export default new Router().match('/', ({ serveStatic }) => {
-  serveStatic('./index.html');
-});
+export default new Router()
+  .match('/', ({ serveStatic }) => {
+    serveStatic('./index.html');
+  })
 
-// uncomment to get /__xdn__/cache-manifest.js to fail
-// .fallback(({ appShell }) => {
-//   appShell('./index.html');
-// });
+  // uncomment to get /__xdn__/cache-manifest.js to fail
+  .fallback(({ appShell }) => {
+    appShell('./index.html');
+  });
